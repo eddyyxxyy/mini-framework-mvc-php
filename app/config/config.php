4e016bfa914ef8c0689621a6cb98e13c8ca88adc
Edson Pimenta <edson.tibo@gmail.com>
2023-01-23 10:08:03 -0300

    Build: creating project structure

---
+++ app/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('BASE', '/mini-framework-mvc/');
+define('DEBUG_URI_COUNT', true);
+define('UNSET_URI_COUNT', 1);
---
+++ app/controller/TestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controller;
+
+class TestController
+{
+    public function seta()
+    {
+        return ['nome' => 'Edson'];
+    }
+}
