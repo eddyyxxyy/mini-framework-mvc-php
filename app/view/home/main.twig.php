80164611a18403a76d1182396bdd26d8683df2a9
Edson Pimenta <edson.tibo@gmail.com>
2023-01-25 10:29:42 -0300

    Adds style, h1 and p to home page div

--- app/view/home/main.twig.php
+++ app/view/home/main.twig.php
@@ -1,8 +1,19 @@
 {% extends 'partials/body.twig.php' %}
+
 {% block title %}Home - Mini Framework{% endblock %}
 
 {% block body %}
-<div>
-    <h1>TEST</h1>
+<div class="center-screen max-width padding bg-white">
+    <h1>Home Page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
 </div>
 {% endblock %}
