305c8cf4f9a4134c1074feaeb69928105ded5d39
Edson Pimenta <edson.tibo@gmail.com>
2023-01-25 11:55:19 -0300

    Feat: creates pages views.

---
+++ app/view/zipcode/main.twig.php
@@ -0,0 +1,19 @@
+{% extends 'partials/body.twig.php' %}
+
+{% block title %}Zipcode - Mini Framework{% endblock %}
+
+{% block body %}
+<div class="center-screen max-width padding bg-white">
+    <h1>Zipcode</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nostrum distinctio. Deserunt omnis atque rem reprehenderit excepturi rerum animi, ducimus nobis libero. Qui alias beatae sed asperiores error fugiat tenetur?</p>
+</div>
+{% endblock %}
