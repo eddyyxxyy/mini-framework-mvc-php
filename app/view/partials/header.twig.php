28a23a1ba53d7e7b50ed1c1df822c83800599de9
Edson Pimenta <edson.tibo@gmail.com>
2023-01-25 11:56:10 -0300

    Refactor: changes category nav-item to zipcode

--- app/view/partials/header.twig.php
+++ app/view/partials/header.twig.php
@@ -13,7 +13,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{BASE}}category/">Categories</a>
+          <a class="nav-link" href="{{BASE}}zipcode/">Zipcode</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{BASE}}about-us/">About Us</a>
