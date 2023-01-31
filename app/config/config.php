e85f25acc43e875f87c4f874301cae67e6b66496
Edson Pimenta <edson.tibo@gmail.com>
2023-01-31 11:56:43 -0300

    Feat: defines env variables

--- app/config/config.php
+++ app/config/config.php
@@ -14,3 +14,8 @@
 define('BASE', '/mini-framework-mvc/');
 define('DEBUG_URI', false);
 define('UNSET_URI_COUNT', 1);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'eddyxide');
+define('DB_PASS', '123!leandoer');
+define('DB_NAME', 'mini_framework');
