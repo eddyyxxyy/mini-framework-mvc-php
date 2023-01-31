5c2909a40207d1046d7e8c9c6c88930144cf2c67
Edson Pimenta <edson.tibo@gmail.com>
2023-01-31 20:02:29 -0300

    Feat: implements Products Model

--- app/model/ProductModel.php
+++ app/model/ProductModel.php
@@ -10,34 +10,76 @@ class ProductModel
 
     public function __construct()
     {
-        $this->pdo = new Model();
+        $this->_pdo = new Model();
     }
 
     public function insert(Object $params)
     {
-        $sql = '';
+        $sql = 'INSERT INTO products (name, image, description) VALUES (:name, :image, :description)';
 
         $params = [
-            '' => '',
+            ':name' => $params->name,
+            ':image' => $params->image,
+            ':description' => $params->description,
         ];
+
+        if (!$this->_pdo->executeNonQuery($sql, $params)) {
+            return -1;
+        }
+
+        return $this->_pdo->getLastID();
     }
 
     public function update(Object $params)
     {
-        $sql = '';
+        $sql = 'UPDATE products SET name = :name, image = :image, \
+            description = :description WHERE id = :id';
 
         $params = [
-            '' => '',
+            ':id' => $params->id,
+            ':name' => $params->name,
+            ':image' => $params->image,
+            ':description' => $params->description,
         ];
+
+        return $this->_pdo->executeNonQuery($sql, $params);
     }
 
     public function getAll()
     {
+        $sql = 'SELECT id, name, image, description FROM products ORDER BY name ASC';
 
+        $dt = $this->_pdo->executeQuery($sql);
+
+        $productsList = null;
+
+        foreach ($dt as $dr) {
+            $productsList[] = $this->_collection($dr);
+        }
+
+        return $productsList;
     }
 
     public function getById(int $id)
     {
+        $sql = 'SELECT id, name, image, description FROM products WHERE id = :id';
 
+        $param = [
+            ':id' => $id,
+        ];
+
+        $dr = $this->_pdo->executeQueryOneRow($sql, $param);
+
+        return $this->_collection($dr);
+    }
+
+    private function _collection($param)
+    {
+        return (object)[
+            'id' => $param['id'] ?? null,
+            'name' => $param['name'] ?? null,
+            'image' => $param['image'] ?? null,
+            'description' => $param['description'] ?? null,
+        ];
     }
 }
